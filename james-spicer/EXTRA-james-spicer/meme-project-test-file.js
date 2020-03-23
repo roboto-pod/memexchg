0a510e91111cacb57983e64ed456b0c9c40f8621
James Spicer <spicer.r.james@gmail.com>
2020-03-23 16:23:57 -0500

    html and java image filter updates. work in progress, fixing functionality

---
+++ james-spicer/EXTRA-james-spicer/meme-project-test-file.js
@@ -0,0 +1,25 @@
+let brightness = 100;
+let contrast = 100;
+let saturate = 100;
+let grayscale = 0;
+let invert = 0;
+let huerotate = 0;
+let blur = 0;
+let opacity = 100;
+let sepia = 0;
+let dropshadow = 0;
+
+//-----get the image from the html------//
+const imgture = document.getElementById("img");
+
+const slider1 = document.getElementById("slider1");
+const value1 = document.getElementById("bright");
+const slider2 = document.getElementById("slider2");
+const slider3 = document.getElementById("slider3");
+const value3 = document.getElementById("saturate");
+const slider4 = document.getElementById("slider4");
+const value4 = document.getElementById("gray");
+const slider5 = document.getElementById("slider5");
+const value5 = document.getElementById("invert");
+const slider6 = document.getElementById("slider6");
+const value6 = document.getElementById("hue");
